a4d886057ca429cfce6db028022fedebea84eed1
Pedro <pedrohalmeidamendonca@gmail.com>
2023-01-25 19:54:57 -0300

    Tela de perfil finalizada

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function Profile() {
+export default function Profile() {
+  const history = useHistory();
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <div>Profile</div>
+    <>
+      <p data-testid="profile-email">{email}</p>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+      </button>
+    </>
   );
 }
-
-export default Profile;
