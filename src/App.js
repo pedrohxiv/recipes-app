b4aa963d070ee82f729b7cda7f69d4487a9c09a6
Gabriel Sena <gabrsena@hotmail.com>
2023-01-26 13:29:46 -0300

    req02 ao req 06 sem os testes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ import FavoriteRecipes from './pages/FavoriteRecipes';
 import RecipeDetails from './pages/RecipeDetails';
 import Recipes from './pages/Recipes';
 import RecipeInProgress from './pages/RecipeInProgress';
+import AppProvider from './context/AppProvider';
+import Meals from './pages/Meals';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/done-recipes" component={ DoneRecipes } />
-        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/foods/:id" component={ RecipeDetails } />
-        <Route path="/drinks/:id" component={ RecipeDetails } />
-        <Route path="/foods" component={ Recipes } />
-        <Route path="/drinks" component={ Recipes } />
+        <AppProvider>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Meals } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/done-recipes" component={ DoneRecipes } />
+          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+          <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/foods/:id" component={ RecipeDetails } />
+          <Route path="/drinks/:id" component={ RecipeDetails } />
+          <Route path="/foods" component={ Recipes } />
+          <Route path="/drinks" component={ Recipes } />
+        </AppProvider>
       </Switch>
     </div>
   );
