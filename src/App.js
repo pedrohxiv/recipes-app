e8c21ba4723e3b21f0c25aabce3ca903add5ef12
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-01-26 00:48:12 -0300

    alterei browser router para app e fiz testes para o header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
@@ -13,20 +13,22 @@ import { RecipeProvider } from './context/RecipeContext';
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/done-recipes" component={ DoneRecipes } />
-        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route path="/meals/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-        <Route path="/meals/:id" component={ RecipeDetails } />
-        <Route path="/drinks/:id" component={ RecipeDetails } />
-        <RecipeProvider>
-          <Route path="/meals" component={ Recipes } />
-          <Route path="/drinks" component={ Recipes } />
-        </RecipeProvider>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/done-recipes" component={ DoneRecipes } />
+          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+          <Route path="/meals/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/meals/:id" component={ RecipeDetails } />
+          <Route path="/drinks/:id" component={ RecipeDetails } />
+          <RecipeProvider>
+            <Route path="/meals" component={ Recipes } />
+            <Route path="/drinks" component={ Recipes } />
+          </RecipeProvider>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import Search from './Search';
 
 function Header(props) {
   const history = useHistory();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const pagePath = history.location.pathname;
+  // const pagesWithoutSearch = ['/profile', '/done-recipes', '/favorite-recipes'];
   const { title } = props;
   return (
     <div>
-      <img
-        data-testid="profile-top-btn"
-        src={ profileIcon }
-        alt="Profile icon"
-      />
-      {/* </button> */}
-      {(pagePath !== '/profile'
-      && pagePath !== '/done-recipes' && pagePath !== '/favorite-recipes') && (
+      <h1 data-testid="page-title">{title}</h1>
+      <button
+        onClick={ () => history.push('/profile') }
+        data-testid="btn-profile"
+      >
         <img
-          data-testid="search-top-btn"
-          src={ searchIcon }
+          data-testid="profile-top-btn"
+          src={ profileIcon }
           alt="Profile icon"
         />
-
+      </button>
+      {(pagePath !== '/profile'
+      && pagePath !== '/done-recipes' && pagePath !== '/favorite-recipes') && (
+        <button
+          onClick={ () => setIsSearchOpen(!isSearchOpen) }
+          data-testid="btn-show-search"
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="Profile icon"
+          />
+        </button>
       )}
-      <h1 data-testid="page-title">{title}</h1>
+      {isSearchOpen && <Search />}
     </div>
   );
 }
