00f4f6d777afe22ff3c44853cc6666fd7b7063f4
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-01-31 12:42:50 -0300

    mudei imports do teste do header

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Recipes from '../pages/Recipes';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
 
 describe('Testa o componente Search', () => {
   test('Testa se input search aparece na tela', () => {
-    renderWithRouter(<Recipes />);
-
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/meals');
+    });
     const btnShowSearch = screen.getByTestId('btn-show-search');
     userEvent.click(btnShowSearch);
     const inputSearch = screen.getByTestId('search-input');
     expect(inputSearch).toBeInTheDocument();
   });
   test('Testa icon perfil leva para /profile', () => {
-    const { history } = renderWithRouter(<Recipes />);
+    const { history } = renderWithRouter(<App />);
     const btnProfile = screen.getByTestId('btn-profile');
     userEvent.click(btnProfile);
     expect(history.location.pathname).toBe('/profile');
