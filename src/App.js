6d9821d8fc22aeac89fe0d60e764dfc9ed799ca4
Gabriel Sena <gabrsena@hotmail.com>
2023-01-31 12:22:56 -0300

    correcao erro de lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
@@ -10,7 +9,6 @@ import RecipeDetails from './pages/RecipeDetails';
 import Recipes from './pages/Recipes';
 import RecipeInProgress from './pages/RecipeInProgress';
 import AppProvider from './context/AppProvider';
-import Meals from './pages/Meals';
 
 function App() {
   return (
@@ -19,17 +17,16 @@ function App() {
         <Switch>
           <AppProvider>
             <Route exact path="/" component={ Login } />
-            <Route exact path="/meals" component={ Meals } />
-            <Route path="/profile" component={ Profile } />
-            <Route path="/done-recipes" component={ DoneRecipes } />
-            <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-            <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
-            <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-            <Route path="/foods/:id" component={ RecipeDetails } />
-            <Route path="/drinks/:id" component={ RecipeDetails } />
-            <Route path="/foods" component={ Recipes } />
-            <Route path="/drinks" component={ Recipes } />
           </AppProvider>
+          <Route path="/profile" component={ Profile } />
+          <Route path="/done-recipes" component={ DoneRecipes } />
+          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+          <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+          <Route path="/foods/:id" component={ RecipeDetails } />
+          <Route path="/drinks/:id" component={ RecipeDetails } />
+          <Route path="/foods" component={ Recipes } />
+          <Route path="/drinks" component={ Recipes } />
         </Switch>
       </BrowserRouter>
     </div>
