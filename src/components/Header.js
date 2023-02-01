89c701adbbce6786fad8ed11728ddbb93d0bd448
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-02-01 17:23:35 -0300

    checando pra ver se passa no avaliador

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
-import Search from './Search';
+import Search from './SearchBar';
 
 function Header({ title = 'Meals' }) {
   const history = useHistory();
