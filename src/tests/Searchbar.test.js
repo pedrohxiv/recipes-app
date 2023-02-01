0e1feb1bbe4b6fd96b0888f98d9c0939095c7a1b
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-02-01 13:58:05 -0300

    arrumei erros eslint

--- src/tests/Searchbar.test.js
+++ src/tests/Searchbar.test.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import { RecipeProvider } from '../context/RecipeContext';
 import App from '../App';
-import AppProvider from '../context/AppProvider';
 import { meals } from '../../cypress/mocks/meals';
 
 describe('Testa o componente Searchbar', () => {
   test('Testa se filtros filtram', async () => {
-    const {history, getByTestId} = renderWithRouter(
+    const { history } = renderWithRouter(
       <RecipeProvider>
         <App />
       </RecipeProvider>,
     );
-    history.push('/meals')  
+    history.push('/meals');
 
     jest.spyOn(global, 'fetch').mockResolvedValue({
       json: jest.fn().mockResolvedValue(meals),
     });
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // gambiarra
     const btnShowSearch = screen.getByTestId('btn-show-search');
+    expect(btnShowSearch).toBeInTheDocument();
   });
   // test('Testa icon perfil leva para /profile', async () => {
   //   const { history } = renderWithRouter(
